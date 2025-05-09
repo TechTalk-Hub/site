458c8ee2ee8c16be786f3359d12f8bbad731c98e
zepang <ummlq4@gmail.com>
2025-05-09 17:44:06 +0800

    feat: 博文列表删除作者信息

--- src/components/PostItemList.tsx
+++ src/components/PostItemList.tsx
@@ -12,15 +12,11 @@ export default function PostItem(props: { posts: Record<string, any>[] }) {
               <PostItemHoverOverlay />
               <div className="grid md:grid-cols-2 gap-5 md:gap-10 items-center">
                 <img className="object-cover rounded-xl" src={post.data.image.url.src} alt="" />
-                <section>
-                  <h3 className="text-3xl font-semibold leading-snug tracking-tight">{post.data.title}</h3>
-                  <div className="flex gap-2 mt-3">
-                    <span className="text-gray-400">
-                      {post.data.author}
-                    </span>
-                    <span className="text-gray-400">• </span>
+                <section className="flex flex-col gap-4">
+                  <h3 className="text-2xl font-semibold leading-snug tracking-tight">{post.data.title}</h3>
+                  <div className="flex flex-col gap-2 mt-3">
                     <time
-                      className="text-gray-400"
+                      className="text-gray-400 text-sm"
                       dateTime={post.data.pubDate.toISOString()}>
                       {post.data.pubDate.toDateString()}
                     </time>
