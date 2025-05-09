b06f2f1e4d772145c8bb4f1bb39fc803c379fd32
zepang <ummlq4@gmail.com>
2025-05-09 16:15:56 +0800

    feat: 添加remark插件给frontmatter添加阅读时间和字数字段，添加文章详情页面的标题和文章元信息展示

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,21 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import { remarkReadingTime } from './src/plugins/remark/readingTime.mjs';
+import { remarkWords } from './src/plugins/remark/readingWords.mjs';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+
+  markdown: {
+    remarkPlugins: [
+      remarkReadingTime,
+      remarkWords
+    ]
+  },
 });
