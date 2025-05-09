c3813581f4c7f8224254dcdc06d24a02c1fd909a
zepang <ummlq4@gmail.com>
2025-05-09 18:03:59 +0800

    fix: 调整博文列表项样式

--- src/components/PostItemList.tsx
+++ src/components/PostItemList.tsx
@@ -10,10 +10,10 @@ export default function PostItem(props: { posts: Record<string, any>[] }) {
           <li>
             <a href={`/posts/${post.id}`} className="relative py-8 block">
               <PostItemHoverOverlay />
-              <div className="grid md:grid-cols-2 gap-5 md:gap-10 items-center">
-                <img className="object-cover rounded-xl" src={post.data.image.url.src} alt="" />
-                <section className="flex flex-col gap-4">
-                  <h3 className="text-2xl font-semibold leading-snug tracking-tight">{post.data.title}</h3>
+              <div className="grid md:grid-cols-[1fr_2fr] gap-5 md:gap-10 items-center">
+                <img className=" object-contain rounded-lg min-w-40 min-h-40" src={post.data.image.url.src} alt="" />
+                <section className="flex flex-col gap-4 w-full max-w-96">
+                  <h3 className="text-2xl font-semibold leading-snug tracking-tight truncate">{post.data.title}{post.data.title}</h3>
                   <div className="flex flex-col gap-2 mt-3">
                     <time
                       className="text-gray-400 text-sm"
